573a837d84e965b4de45731713ae1e21ce02d03d
dunglas <dunglas@c3c03a9e-5c50-0410-9881-e1bd45efd7ca>
2008-08-17 19:19:38 +0000

    MySQL support and modularization

--- hosting.py
+++ hosting.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python2.5
-# vim: tabstop=4 expandtab shiftwidth=4
+# vim: set fileencoding=utf-8 :
+# Copyright (C) 2008 Kévin Dunglas <dunglas@gmail.com>
+#
+# Authors:
+#  Kévin Dunglas
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA 02111-1307 USA
+
 import sys
-import os
-import subprocess
-import shutil
-import crypt
-import random
 from optparse import OptionParser
 import getpass
+from hosting.Unix import Unix
+from hosting.Mysql import Mysql
+from hosting.Apache import Apache
+from hosting.Utils import Utils
 
-# Round Cube webmail user creation
-#CREATE USER 'roundcube'@ 'localhost' IDENTIFIED BY '****************';
-#GRANT USAGE ON * . * TO 'roundcube'@ 'localhost' IDENTIFIED BY '****************' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
-#CREATE DATABASE `roundcube` ;
-#GRANT ALL PRIVILEGES ON `roundcube` . * TO 'roundcube'@ 'localhost';
+APPNAME    = "hosting.py"
+APPVERSION = "0.2"
 
-APPNAME="hosting.py"
-APPVERSION="0.1"
+MYSQL_HOST   = "localhost"
+MYSQL_USER   = "root"
+MYSQL_PASSWD = ""
 
-BASEDIR = "/home"
-SKEL = "/etc/skel-www"
-AVAILABLE = "/etc/apache2/sites-available"
-#ENABLED = "/etc/apache2/sites-enabled"
-SHELL = "/bin/false"
-DOMAIN = "lapin-blanc.net"
-ADMIN = "contact@lapin-blanc.net"
-VHOST = """<VirtualHost *>
+BASEDIR      = "/home"
+SKEL         = "/etc/skel-www"
+AVAILABLE    = "/etc/apache2/sites-available"
+#ENABLED     = "/etc/apache2/sites-enabled"
+SHELL        = "/bin/false"
+DOMAIN       = "lapin-blanc.net"
+ADMIN        = "contact@lapin-blanc.net"
+VHOST        = """<VirtualHost *>
         ServerName %(domain)s
         ServerAdmin %(email)s
         
@@ -45,123 +61,6 @@ VHOST = """<VirtualHost *>
 </VirtualHost>"""
 
 
-class Mysql:
-    """Manage MySQL accounts and databases"""
-    def __init__(self, username):
-        self.username = username
-    
-    def add(self, password=None):
-        pass
-    
-    def modify(self, password=None):
-        pass
-    
-    def delete(self):
-        pass
-
-
-class Site:
-    """Manage Apache virtuals hosts"""
-    def __init__(self, name):
-        self.name = name
-
-    def add(self, email=None, domain=None):
-        """Create a vhost"""
-        if email is None:
-            email = ADMIN
-        if domain is None:
-            domain = "%s.%s" % (self.name, DOMAIN)
-        home = os.path.join(BASEDIR, self.name)
-        document_root = os.path.join(home, "public_html")
-        logs = os.path.join(home, "logs")
-        vhost = VHOST % {"domain": domain, "email": email, "home": home,\
-            "document_root": document_root, "logs": logs}
-        file = os.path.join(AVAILABLE, self.name)
-        f = open(file, "w")
-        f.write(vhost)
-        f.close()
-        self.enable()
-
-    def enable(self):
-        """Enable a vhost"""
-        subprocess.check_call(["a2ensite", self.name])
-
-    def disable(self):
-        """Disable a vhost"""
-        subprocess.check_call(["a2dissite", self.name])
-
-    def modify(self, email=None, domain=None):
-        """Modify a vhost"""
-        self.delete()
-        self.add(email, domain)
-    
-    def delete(self):
-        """Delete a vhost"""
-        self.disable()
-        file = os.path.join(AVAILABLE, self.name)
-        os.unlink(file)
-
-
-class Account:
-    """Manage UNIX accounts"""
-    def __init__(self, username):
-        self.username = username
-
-    def add(self, password, shell=None, email=None, domain=None):
-        """Add an account"""
-
-        comment = "Web account for "
-        #sep = False
-        #if email is not None:
-        #    comment = comment + email
-        #    sep = True
-        #if domain is not None:
-        #    if sep:
-        #        comment = comment + " - "
-        #    comment = comment + domain
-
-        comment = comment + self.username
-        
-        if email is not None:
-            comment = comment + "(%s" % email
-        if domain is not None:
-            comment = comment + " - %s)" % domain
-        elif email is not None:
-            comment = comment + ")"
-        
-        password = Main.password(password)
-        if shell is None:
-            shell = SHELL
-        # We don't use -b (--base-dir) because it seems to have a blocking bug in the etch's useradd
-        subprocess.check_call(["useradd","-c", comment, "-m", "-d", os.path.join(BASEDIR, self.username),\
-        	"-k", SKEL, "-s", shell, "-p", password[1],  self.username])
-        return password[0]
-
-    def change_password(self, password=None):
-        """Change an account's password"""
-        password = self.password(password)
-        subprocess.check_call(["usermod", "-p", password[1], self.username])
-        return password[0]
-
-    def change_shell(self, shell):
-        """Change an account's shell"""
-        subprocess.check_call(["usermod", "-s", shell, self.username])
-
-    def modify(self, password=None, shell=None):
-        """Modify an account"""
-        if password is not None:
-            self.change_password(password)
-        if shell is not None:
-            self.change_shell(shell)
-
-    def delete(self, delete_data=False):
-        """Delete an account"""
-        subprocess.check_call(["userdel", self.username])
-        
-        if delete_data:
-            shutil.rmtree("%s/%s" % (BASEDIR, self.username))
-
-
 class Main:
     """Main program"""
     def __init__(self):
@@ -170,8 +69,8 @@ Commands:
 	add		Add an account
 	del		Delete an account
 	mod		Modify account""", version="%prog " + APPVERSION)
-        parser.add_option("-p", "--password", action="store_true",\
-            dest="password", help="ask for password")
+        parser.add_option("-p", "--passwd", action="store_true",\
+            dest="passwd", help="ask for passwd")
         parser.add_option("-b", "--batch", action="store_true",\
             default=False, dest="batch", help="batch mode")  
         parser.add_option("-s", "--shell", dest="shell",\
@@ -186,59 +85,47 @@ Commands:
             parser.error("Require two arguments")
         command = args[0]
         username = args[1]
-        a = Account(username)
-        s = Site(username)
+        u = Unix(username, BASEDIR, SKEL, SHELL)
+        a = Apache(username, VHOST, AVAILABLE, BASEDIR, ADMIN, DOMAIN)
+        m = Mysql(username, MYSQL_HOST, MYSQL_USER, MYSQL_PASSWD)
+        
         if command == "add":
-            if options.password:
-                password = self.getpass(options.batch)
+            print "Add..."
+            if options.passwd:
+                passwd = self.getpass(options.batch)
             else:
-                password = None
-            pwd = a.add(password, options.shell, options.email, options.domain)
-            s.add(options.email, options.domain)
-            print "Login: %s" % username
-            print "Password: %s" % pwd
-            print "Domain: %s.%s" % (username, DOMAIN)
+                passwd = Utils.random_passwd()
+
+            u.add(passwd, options.shell, options.email, options.domain)
+            a.add(options.email, options.domain)
+            m.add(passwd)
+            
         elif command == "mod":
-            if options.password:
-                password = self.getpass(options.batch)
+            if options.passwd:
+                passwd = self.getpass(options.batch)
             else:
-                password = None
-            a.modify(password, options.shell)
-            s.modify(options.email, options.domain)
+                passwd = Utils.random_passwd()
+            
+            u.modify(passwd, options.shell)
+            a.modify(options.email, options.domain)
+            m.modify(passwd)
         elif command == "del":
+            u.delete()
             a.delete()
-            s.delete()
+            m.delete()
         else:
             parser.error("Unknow command")
         
-    def getpass(batch=False):
+    def getpass(self, batch=False):
         """Get a password"""
         if batch:
-            return raw_input("Password:")
+            return raw_input("passwd:")
         try:
             return getpass.getpass()
         except:
             print "Error: invalid argument"
             print "Try using --batch"
             sys.exit(2)
-    
-    @staticmethod
-    def password(password=None):
-        """Encrypt a password using a random 2 chars salt.
-        If the password is None generate a 8 chars long."""
-        li = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890./"
-
-        if password is None:
-            password = str()
-            for i in range(8):
-                password = password + str(random.choice(li))
-
-        salt = str()
-        for i in range(2):
-            salt = salt + str(random.choice(li))
-        
-        return (password, crypt.crypt(password, salt))
-
 
 if __name__ == "__main__":
     Main()
